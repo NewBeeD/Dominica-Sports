38a4409fcf2980be3d597a8c492ed65b238ad8e7
Danphil Daniel <danieldanphil@gmail.com>
2023-04-26 19:06:45 -0400

    Menu Items

--- frontend/src/components/homepage/NavBar.js
+++ frontend/src/components/homepage/NavBar.js
@@ -1,8 +1,26 @@
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button, Menu, MenuItem} from '@mui/material'
 import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+
+import { useState } from 'react';
 
 
 export const NavBar = () => {
+
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [open, setOpen] = useState(false)
+
+  const handleClose = () => {
+    setAnchorEl(null)
+    setOpen(false)
+  }
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget)
+    setOpen(true)
+  }
+
 
 
   return (
@@ -13,11 +31,29 @@ export const NavBar = () => {
         <Typography variant='h6' component='div' sx={{flexGrow: 1}}>Sports Dominica</Typography>
 
 
-        <Stack direction='row' spacing={2} sx={{ flexGrow: 1, border: '2px solid red'}}>
-          <Button color='inherit'>DFA</Button>
-          <Button color='inherit'>DBA</Button>
-          <Button color='inherit'>DCA</Button>
+        <Stack direction='row' spacing={2} sx={{ flexGrow: 1}}>
+
+          <Button 
+          color='inherit' 
+          id='dfa'
+          onClick={handleClick}
+          endIcon={<KeyboardArrowDownIcon />}>DFA</Button>
+          
+          
+          <Button color='inherit' id='dba'
+          endIcon={<KeyboardArrowDownIcon />}>DBA</Button>
+          <Button color='inherit' id='dca'
+          endIcon={<KeyboardArrowDownIcon />}>DCA</Button>
         </Stack>
+
+        <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}>
+
+          <MenuItem onClick={handleClose}>Table</MenuItem>
+          <MenuItem onClick={handleClose}>Fixtures</MenuItem>
+        </Menu>
 
       </Toolbar>
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
+
+import {BrowserRouter as Router } from 'react-router-dom'
+
+
+// Apollo Client
+const client = new ApolloClient({
+
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <Router>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Router>
+
   </React.StrictMode>
 );
 
